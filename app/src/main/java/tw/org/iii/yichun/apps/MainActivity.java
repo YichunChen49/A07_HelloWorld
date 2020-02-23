eef08b078ebf1c17d6ef636ae5c91e91bc74d7df
yichun <yichunchen.49@gmail.com>
2020-02-23 16:05:07 +0800

    02231604

--- app/src/main/java/tw/org/iii/yichun/apps/MainActivity.java
+++ app/src/main/java/tw/org/iii/yichun/apps/MainActivity.java
@@ -3,13 +3,31 @@ package tw.org.iii.yichun.apps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+
+    }
+
+    public void click(View view) {
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size() < 6){
+            set.add((int)(Math.random()*49+1));
+        }
+        //Log.v("Yichun", set.toString());
+        tv.setText(set.toString());
 
     }
 }
